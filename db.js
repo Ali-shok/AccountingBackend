4acf53699f12b17276a17e62d37c896ec954cefc
Ali Shok <shok13860@gmail.com>
2023-07-10 00:56:02 -0700

    swdsQDWQSXSade

--- db.js
+++ db.js
@@ -1,2 +1,25 @@
+const mysql = require('mysql2');
+require('dotenv').config();
 
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    process.exit(1);
+  }
+  console.log('Connected to the database.');
+
+});
+
+
+<<<<<<< HEAD
 module.exports = pool;
+=======
+module.exports = pool;
+>>>>>>> f2ce08cacac44a3589f5ad871365bb48d0cf367b
