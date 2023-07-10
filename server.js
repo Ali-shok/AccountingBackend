8426a8c7b4534a8f59c0ee00536cfa1f6c916839
Ali Shok <shok13860@gmail.com>
2023-07-10 00:51:04 -0700

    swdsQDWQSXSade

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const db = require('./db');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+
+
