979d2b68f93ebc966622c47a54e0b137232db490
Ali Shok <shok13860@gmail.com>
2023-07-10 01:37:04 -0700

    swdsQDWQSXSade

---
+++ routers/users.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+
+
+router.get('/admin', (req, res) => {
+    db.query('SELECT * FROM `mydatabase`.`admin`', (err, rows) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        res.json(rows);
+      }
+    });
+  });
+
+
+  router.get('/employee', (req, res) => {
+    db.query('SELECT * FROM `mydatabase`.`employee`', (err, rows) => {
+      if (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+      } else {
+        res.json(rows);
+      }
+    });
+  });
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,33 +1,28 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const payments = require("./routers/payments");
+const user = require("./routers/users");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use('/',payments);
+app.use('/',user);
 
 
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-  });
+  console.log('Server is running on port 3000');
+});
+
+
+
+
+
+
+
+
+
+
 
 
